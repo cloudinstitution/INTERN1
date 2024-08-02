3d53a45c32a5f6fd7f0982e738f72daf7c0f2afa
cloudinstitution <cloudinstitution@gmail.com>
2024-08-02 15:27:23 +0530

    commited

--- DEMO.java
+++ DEMO.java
@@ -1,5 +1,8 @@
 class DEMO{
      public static void main(String[] args) {
-        System.out.println("hello world ");
+        int a=10;
+	int b=20;
+	int c=a+c;
+	     System.out.println(c);
      }
 }
